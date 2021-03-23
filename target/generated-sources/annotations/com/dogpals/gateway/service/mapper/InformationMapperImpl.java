59f7d3ec4cb7c3fda4a4feb67dbab2e62b9156e6
pmk <pmkhai@gmail.com>
2021-03-23 17:22:29 +0800

    added missing mvn files

--- target/generated-sources/annotations/com/dogpals/gateway/service/mapper/InformationMapperImpl.java
+++ target/generated-sources/annotations/com/dogpals/gateway/service/mapper/InformationMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-03-22T01:02:15+0800",
+    date = "2021-03-22T22:44:05+0800",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11 (Oracle Corporation)"
 )
 @Component
