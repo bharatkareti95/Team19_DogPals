49fd58c4b150b2aaf56a5602bc7caf484cbfdb5a
Pau001 <45557098+Pau001@users.noreply.github.com>
2021-05-05 21:54:23 +0800

    comment information

--- dogPals/src/main/java/com/dogpals/gateway/service/mapper/InformationMapper.java
+++ dogPals/src/main/java/com/dogpals/gateway/service/mapper/InformationMapper.java
@@ -1,25 +1,25 @@
-package com.dogpals.gateway.service.mapper;
+// package com.dogpals.gateway.service.mapper;
 
 
-import com.dogpals.gateway.domain.*;
-import com.dogpals.gateway.service.dto.InformationDTO;
+// import com.dogpals.gateway.domain.*;
+// import com.dogpals.gateway.service.dto.InformationDTO;
 
-import org.mapstruct.*;
+// import org.mapstruct.*;
 
-/**
- * Mapper for the entity {@link Information} and its DTO {@link InformationDTO}.
- */
-@Mapper(componentModel = "spring", uses = {})
-public interface InformationMapper extends EntityMapper<InformationDTO, Information> {
+// /**
+//  * Mapper for the entity {@link Information} and its DTO {@link InformationDTO}.
+//  */
+// @Mapper(componentModel = "spring", uses = {})
+// public interface InformationMapper extends EntityMapper<InformationDTO, Information> {
 
 
 
-    default Information fromId(Long id) {
-        if (id == null) {
-            return null;
-        }
-        Information information = new Information();
-        information.setId(id);
-        return information;
-    }
-}
+//     default Information fromId(Long id) {
+//         if (id == null) {
+//             return null;
+//         }
+//         Information information = new Information();
+//         information.setId(id);
+//         return information;
+//     }
+// }
