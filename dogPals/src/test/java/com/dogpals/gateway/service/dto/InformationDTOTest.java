c13e744194f1a4e63854e4963524bec88a394472
Pau001 <45557098+Pau001@users.noreply.github.com>
2021-05-05 22:59:47 +0800

    comment information

--- dogPals/src/test/java/com/dogpals/gateway/service/dto/InformationDTOTest.java
+++ dogPals/src/test/java/com/dogpals/gateway/service/dto/InformationDTOTest.java
@@ -1,23 +1,23 @@
-package com.dogpals.gateway.service.dto;
+// package com.dogpals.gateway.service.dto;
 
-import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
-import com.dogpals.gateway.web.rest.TestUtil;
+// import org.junit.jupiter.api.Test;
+// import static org.assertj.core.api.Assertions.assertThat;
+// import com.dogpals.gateway.web.rest.TestUtil;
 
-public class InformationDTOTest {
+// public class InformationDTOTest {
 
-    @Test
-    public void dtoEqualsVerifier() throws Exception {
-        TestUtil.equalsVerifier(InformationDTO.class);
-        InformationDTO informationDTO1 = new InformationDTO();
-        informationDTO1.setId(1L);
-        InformationDTO informationDTO2 = new InformationDTO();
-        assertThat(informationDTO1).isNotEqualTo(informationDTO2);
-        informationDTO2.setId(informationDTO1.getId());
-        assertThat(informationDTO1).isEqualTo(informationDTO2);
-        informationDTO2.setId(2L);
-        assertThat(informationDTO1).isNotEqualTo(informationDTO2);
-        informationDTO1.setId(null);
-        assertThat(informationDTO1).isNotEqualTo(informationDTO2);
-    }
-}
+//     @Test
+//     public void dtoEqualsVerifier() throws Exception {
+//         TestUtil.equalsVerifier(InformationDTO.class);
+//         InformationDTO informationDTO1 = new InformationDTO();
+//         informationDTO1.setId(1L);
+//         InformationDTO informationDTO2 = new InformationDTO();
+//         assertThat(informationDTO1).isNotEqualTo(informationDTO2);
+//         informationDTO2.setId(informationDTO1.getId());
+//         assertThat(informationDTO1).isEqualTo(informationDTO2);
+//         informationDTO2.setId(2L);
+//         assertThat(informationDTO1).isNotEqualTo(informationDTO2);
+//         informationDTO1.setId(null);
+//         assertThat(informationDTO1).isNotEqualTo(informationDTO2);
+//     }
+// }
