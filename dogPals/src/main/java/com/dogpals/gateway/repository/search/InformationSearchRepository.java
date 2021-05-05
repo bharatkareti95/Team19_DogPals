0360fa3a7758a17f9a238bfc3225653c58b47616
Pau001 <45557098+Pau001@users.noreply.github.com>
2021-05-05 21:50:27 +0800

    comment information

--- dogPals/src/main/java/com/dogpals/gateway/repository/search/InformationSearchRepository.java
+++ dogPals/src/main/java/com/dogpals/gateway/repository/search/InformationSearchRepository.java
@@ -1,11 +1,11 @@
-package com.dogpals.gateway.repository.search;
+// package com.dogpals.gateway.repository.search;
 
-import com.dogpals.gateway.domain.Information;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+// import com.dogpals.gateway.domain.Information;
+// import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 
-/**
- * Spring Data Elasticsearch repository for the {@link Information} entity.
- */
-public interface InformationSearchRepository extends ElasticsearchRepository<Information, Long> {
-}
+// /**
+//  * Spring Data Elasticsearch repository for the {@link Information} entity.
+//  */
+// public interface InformationSearchRepository extends ElasticsearchRepository<Information, Long> {
+// }
